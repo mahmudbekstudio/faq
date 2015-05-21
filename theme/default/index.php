110813cdff21ad54fb735516cd351170a96f0bb3
Mahmudbek Odilov <modilov@dealerfire.com>
2015-05-21 10:26:20 +0200

    main template, css and js files

--- theme/default/index.php
+++ theme/default/index.php
@@ -1,16 +1,71 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>&nbsp;</title>
 	<meta name="keywords" content="keyword" />
 	<meta name="description" content="description" />
-	<link href="css/styles.css" type="text/css" rel="stylesheet" />
-	<script type="text/javascript" src="js/scripts.js"></script>
+	<link href="/css/reset.css" rel="stylesheet" />
+	<link href="/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="/css/styles.css" rel="stylesheet" />
 </head>
 <body>
-<?php
-echo $content;
-?>
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">FAQ.UZ</a>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="#contact">Contact</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li class="divider"></li>
+						<li class="dropdown-header">Nav header</li>
+						<li><a href="#">Separated link</a></li>
+						<li><a href="#">One more separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div>
+</nav>
+
+<!-- Begin page content -->
+<div class="container">
+	<?php
+	echo $content;
+	?>
+	<?php /*
+	<div class="page-header">
+		<h1>Sticky footer with fixed navbar</h1>
+	</div>
+	<p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
+	<p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+	*/ ?>
+</div>
+
+<footer class="footer">
+	<div class="container">
+		<p class="text-muted">&copy; faq.uz</p>
+	</div>
+</footer>
+<script src="/js/jquery-2.1.4.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/scripts.js"></script>
 </body>
 </html>
